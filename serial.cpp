7e0a086d235ffba2076dd7b1a49899072accc3bc
Keith Papa <kapru@br006.pvt.bridges.psc.edu>
2018-04-05 18:34:50 -0400

    Update serial

--- serial.cpp
+++ serial.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
-#include "common.h"
+#include "bucket.cpp"
 
 //
 //  benchmarking program
@@ -29,6 +29,9 @@ int main(int argc, char **argv) {
 	FILE *fsave = savename ? fopen(savename, "w") : NULL;
 	FILE *fsum = sumname ? fopen(sumname, "a") : NULL;
 
+	particle_t *local = (particle_t*) malloc(n * sizeof(particle_t));
+	particle_t *local_ghost = (particle_t*) malloc(n * sizeof(particle_t));
+
 	particle_t *particles = (particle_t*) malloc(n * sizeof(particle_t));
 	set_size(n);
 	init_particles(n, particles);
@@ -38,19 +41,48 @@ int main(int argc, char **argv) {
 	//
 	double simulation_time = read_timer();
 
+	std::vector < std::vector<particle_t> > particle_vec;
+	std::vector < std::vector<particle_t> > ghost_vec;
+
+	const int BUCKET_COUNT = 50;
 	for (int step = 0; step < NSTEPS; step++) {
 		navg = 0;
 		davg = 0.0;
 		dmin = 1.0;
+		
+		// Create bucket
+		create_buckets(particles, n, BUCKET_COUNT, particle_vec, ghost_vec);
+
 		//
 		//  compute forces
 		//
-		for (int i = 0; i < n; i++) {
-			particles[i].ax = particles[i].ay = 0;
-			for (int j = 0; j < n; j++) {
-				apply_force(particles[i], particles[j], &dmin, &davg, &navg);
+		for(int curr_bucket = 0; curr_bucket < (int)particle_vec.size(); curr_bucket++){
+			local = particle_vec[curr_bucket].data();
+			local_ghost = ghost_vec[curr_bucket].data();
+			for (int i = 0; i < (int)particle_vec[curr_bucket].size(); i++) {
+				local[i].ax = local[i].ay = 0;
+				// Apply from fellow local
+				for (int j = 0; j < (int)particle_vec[curr_bucket].size(); j++) {
+					apply_force(local[i], local[j], &dmin, &davg, &navg);
+				}
+				// Apply from ghosts
+				for (int j = 0; j < (int)ghost_vec[curr_bucket].size(); j++) {
+					apply_force(local[i], local_ghost[j], &dmin, &davg, &navg);
+				}
 			}
 		}
+
+
+		//
+		// Receive all completed particles
+		//
+		int counter = 0;
+		for(int curr_bucket = 0; curr_bucket < (int)particle_vec.size(); curr_bucket++){
+			memcpy(&particles[counter], particle_vec[curr_bucket].data(), particle_vec[curr_bucket].size() * sizeof(particle_t));
+			counter += particle_vec[curr_bucket].size();
+			//printf("Receive %d particles from slave %d\n", (int)particle_vec[curr_slave - 1].size(), curr_slave);
+		}
+
 
 		//
 		//  move particles
@@ -58,6 +90,7 @@ int main(int argc, char **argv) {
 		for (int i = 0; i < n; i++) {
 			move(particles[i]);
 		}
+
 
 		if (find_option(argc, argv, "-no") == -1) {
 
